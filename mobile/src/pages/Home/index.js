49d7a7f60e2dc545993f0743428182e2e1ad3253
Diorgeles <diorgelesdias@gmail.com>
2020-06-15 20:41:43 -0300

    helps hooks

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,26 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, KeyboardAvoidingView, StyleSheet, Text, Image, TextInput, TouchableOpacity, ScrollView} from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
 
 import api from '../../services/api'
-import Footer from '../Footer' 
+import Footer from '../Footer'
 
+
+/**
+ * BREVE EXPLICAÇÃO DE COMO PENSAR COM HOOKS
+ *
+ * use as funções de fora para mudar estados e deixa O useEffect para escutar a variavel e só então disparar o codigo
+ * que tu quer
+ *
+ *
+ * useState: Seta valor no estado de alguma variavel
+ * useHooks: Escuta a mudança desses estados caso vc passe ele como parametro para o useEffect (tem exemplos abaixo)
+ *
+ * se vc quer que um codigo rode assim que o app abre a tela tu pode fazer um useEffect com parametro vazio, tipo *
+ * useEffect(() => {
+    // aqui dentro vai a logica que tu quer executar assim que a tela carregar
+  }, [])
+ *
+ */
 const Home = () => {
   const[title, setTitle] = useState('')
   const[imdb, setImdb] = useState('')
   const[movies, setMovies] = useState([])
   const[favorites, setFavorites] = useState([])
 
-  async function handleSearch(){
+  //useEffect que fica escutando a mudança do title e movies
+  useEffect(() => {
     const response = await api.get(`/search?title=${title}`)
     setMovies(response.data)
-  }
-  async function handleStar() {
+  }, [title, movies]) // aqui vai os efeito que vc quer ficar escutando mudança
+
+
+  //useEffect que fica escutando a mudança do imdb
+  useEffect(() => {
     const data = {
       imdb
     }
-    await api.post('/movie', data)
+    const response = await api.post('/movie', data)
+    // não sei o que retorna aqui mas isso poderia ser colocado em algum estado
+    console.log("Home -> response", response)
+    setFavorites(setFavorites)
+  }, [imdb, favorites]) // aqui vai os efeito que vc quer ficar escutando mudança
 
+  // essa função fica responsavel apenas por mudar o estado "title"
+  async function handleSearch(value){
+    setTitle(value)
+  }
+  // essa função fica responsavel apenas por mudar o estado "imdbID"
+  async function handleStar(imdbID) {
+    setImdb(imdbID)
   }
 
   return(
@@ -33,16 +65,16 @@ const Home = () => {
             Bem vindo ao mundo espetacular do cinema.
         </Text>
         <View style={styles.searchContainer}>
-          <TextInput 
+          <TextInput
             style={styles.inputSearch}
             value={title}
             onChangeText={setTitle}
           />
-          <TouchableOpacity onPress={(handleSearch)}> 
-            <FontAwesome 
-              name="search" 
-              size={30} 
-              color="#FFF"  
+          <TouchableOpacity onPress={handleSearch}>
+            <FontAwesome
+              name="search"
+              size={30}
+              color="#FFF"
               style={styles.searchIcon}
             />
           </TouchableOpacity>
@@ -53,33 +85,35 @@ const Home = () => {
             {
               movies.Search ? (
                 movies.Search.map(movie =>  (
-                  
+
                   <View key={movie.imdbID} style={styles.list}>
-                    <Image 
-                      style={styles.imageMovie} 
-                      source={{ uri: movie.Poster }} 
+                    <Image
+                      style={styles.imageMovie}
+                      source={{ uri: movie.Poster }}
                     />
                     <Text style={styles.titleMovie}>{movie.Title}</Text>
                     <TouchableOpacity style={styles.star}>
-                      <FontAwesome onPress={() => handleStar(setImdb(movie.imdbID))} size={25} color="#ccc" name="star"/>
+                      {/*PQ a função ta recebendo parametro se na declaração ta vazia?  */}
+                      {/* <FontAwesome onPress={() => handleStar(setImdb(movie.imdbID))} size={25} color="#ccc" name="star"/> */}
+                      <FontAwesome onPress={() => handleStar(movie.imdbID)} size={25} color="#ccc" name="star"/>
                     </TouchableOpacity>
                     <Text style={styles.year}>Ano: { movie.Year }</Text>
                   </View>
                 ))
               ) : <View />
 
-            }    
+            }
         </ScrollView>
       </KeyboardAvoidingView>
       <Footer />
-    </>  
+    </>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 24,        
+        paddingHorizontal: 24,
     },
     title: {
         marginTop: 50,
@@ -102,7 +136,7 @@ const styles = StyleSheet.create({
         color: '#FFF',
         height: 40,
         width: 260,
-        
+
     },
     searchContainer:{
       flexDirection: 'row'
@@ -113,7 +147,7 @@ const styles = StyleSheet.create({
     },
     scroll:{
       marginTop: -40
-      
+
     },
     list:{
       flexDirection: 'column',
@@ -121,7 +155,7 @@ const styles = StyleSheet.create({
       padding: 5,
       backgroundColor: '#FFF',
       height: 60,
-    }, 
+    },
     imageMovie: {
       width: 48,
       height: 48,
@@ -136,10 +170,10 @@ const styles = StyleSheet.create({
       color: '#5F9EA0',
       fontWeight: 'bold'
     },
- 
+
     year: {
       paddingHorizontal: 5,
-      position: 'absolute', 
+      position: 'absolute',
       marginLeft: 70,
       marginTop: 25,
       fontSize: 10
@@ -150,7 +184,7 @@ const styles = StyleSheet.create({
       alignSelf: 'flex-end',
       paddingRight: 20,
       borderRadius: 3,
-    },   
+    },
 })
 
 export default Home
